2f4e6cefe2067309bbc0a72839e58df0f25ab17f
Arno Leruste <recroX91@hotmail.com>
2025-10-28 15:53:57 +0100

    WIP

--- src/salles/C2 - Forêt Interdite.ts
+++ src/salles/C2 - Forêt Interdite.ts
@@ -9,8 +9,6 @@ const LENGTH = 1024;
 
 export function C2_Forêt_Interdite(): void {
     addDarkness(Darkness.MEDIUM); 
-    ScriptApp.showCenterLabel("C2 - Forêt Interdite", WIDTH, LENGTH, 3000);
-    ScriptApp.sayToAll(`C2_Forêt_Interdite`);
     const object = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C2_FORET_INTERDITE.CITROUILLE];
     const object2 = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C2_FORET_INTERDITE.CITROUILLE2];
     const object3 = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C2_FORET_INTERDITE.CITROUILLE3];
--- src/salles/C3_LABYRINTHE.ts
+++ src/salles/C3_LABYRINTHE.ts
@@ -1,6 +1,18 @@
 import { addDarkness } from "../darkness/cameraSombre";
 import { Darkness } from "../darkness/Darkness.1";
+import { HALLOWEEN_OBJECT, HALLOWEEN_OBJECT_KEYS } from "../objects-halloween";
+import { TriggerObjectWithSound } from "../trigger/TriggerObjectWithSound";
 
 export function C3_LABYRINTHE(): void {
   addDarkness(Darkness.EASY); 
+  const object = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C3_LABYRINTHE.CITROUILLE];
+  const object2 = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C3_LABYRINTHE.CITROUILLE2];
+  const object3 = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C3_LABYRINTHE.CITROUILLE3];
+  const object4 = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C3_LABYRINTHE.CITROUILLE4];
+  const object5 = HALLOWEEN_OBJECT[HALLOWEEN_OBJECT_KEYS.C3_LABYRINTHE.CITROUILLE5];
+  TriggerObjectWithSound(object);
+  TriggerObjectWithSound(object2);
+  TriggerObjectWithSound(object3);
+  TriggerObjectWithSound(object4);
+  TriggerObjectWithSound(object5);
 }
